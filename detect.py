449fb7991c8e84c40a31b855f690236505459df5
Lev2307 <Unfortunately1366@gmail.com>
2024-05-16 17:07:22 +0100

    first commit

---
+++ detect.py
@@ -0,0 +1,56 @@
+import sys
+import time
+import threading
+import queue
+
+import cv2
+from tflite_support.task import core
+from tflite_support.task import processor
+from tflite_support.task import vision
+import utils
+
+CAMERA_RTSP_URL = "rtsp://admin_camera:camera_password@192.168.222.105:554/stream2"
+
+bird_detection = 'bird_trained_models/bird_detection/birds_model_lite1.tflite' 
+bird_classification = 'bird_trained_models/bird_classification/10_birds_species_model_lite1.tflite'
+first_retrained_bird_detection_model = "bird_trained_models/retrained_models_for_detection/retrain1_birds_detection_lite0.tflite"
+second_retrained_bird_detection_model = "bird_trained_models/retrained_models_for_detection/retrain2_birds_detection_lite0.tflite"
+
+
+q = queue.Queue()
+
+def initialize_livestream():    
+    # init livestream
+    cap = cv2.VideoCapture(CAMERA_RTSP_URL)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        q.put(frame)
+
+def display_livestream():
+      # init tf models
+      base_options = core.BaseOptions(
+          file_name=first_retrained_bird_detection_model, num_threads=4)
+      detection_options = processor.DetectionOptions(
+          max_results=2, score_threshold=0.5)
+      options = vision.ObjectDetectorOptions(
+          base_options=base_options, detection_options=detection_options)
+      detector = vision.ObjectDetector.create_from_options(options)
+
+      counter = 0
+      while True:
+        if q.empty() != True:
+            frame = q.get()
+            counter += 1
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            input_tensor = vision.TensorImage.create_from_array(rgb_frame)
+            detection_result = detector.detect(input_tensor)
+            frame = utils.visualize(frame, detection_result, counter)
+            cv2.imshow("bird_detector", frame)
+        if cv2.waitKey(15) & 0xFF == ord('q'):
+          break
+
+if __name__ == '__main__':
+    thread1 = threading.Thread(target=initialize_livestream)
+    thread2 = threading.Thread(target=display_livestream)
+    thread1.start()
+    thread2.start()
---
+++ utils.py
@@ -0,0 +1,83 @@
+import os
+import cv2
+import numpy as np
+import random
+from datetime import datetime, timedelta
+
+from tflite_support.task import processor
+import yadisk
+
+_MARGIN = 10  # pixels
+_ROW_SIZE = 10  # pixels
+_FONT_SIZE = 1
+_FONT_THICKNESS = 1
+_TEXT_COLOR = (0, 0, 256) # BGR
+BORDER_COLOR = (114, 128, 250)
+
+BIRDS_FOLDER = '/birds/'
+TIME_BREAK = 900
+LAST_IMAGE_DATETIME_FILE="last_image_datetime_file.txt"
+SYMBOLS_FOR_MAKING_RANDOM_FILE_NAME_SUFFIX = 'zwertyuiopasdfghjklzxcvbnmZQWERTYUIOPASDFGHJKLXCVBNM123456789'
+    
+def upload_image_to_yadisk(image_path):
+    disk = yadisk.YaDisk(token="y0_AgAAAABGH1N8AAsnlQAAAAD4ijGdMRFlxmg8QCKx3cM6PMBHYkJ9GJo")
+    if not disk.exists(BIRDS_FOLDER):
+        disk.mkdir(BIRDS_FOLDER)
+    disk.upload(image_path, BIRDS_FOLDER+image_path)
+    
+
+def get_difference_in_seconds(now, file_path):
+    with open(file_path, 'r+') as f:
+        file_content = f.read()
+        if file_content == "":
+            one_hour_earlier_time = (now - timedelta(hours=1))
+            time_dif = (now - one_hour_earlier_time).total_seconds()
+            one_hour_earlier_time = datetime.strftime(one_hour_earlier_time, "%Y-%m-%d %H:%M:%S")
+            f = f.write(one_hour_earlier_time)
+        else:
+            file_content_time = datetime.strptime(file_content, "%Y-%m-%d %H:%M:%S")
+            time_dif = (now - file_content_time).total_seconds()
+            f.close()
+    return time_dif
+
+def create_image(img_path, image):
+    cv2.imwrite(img_path, image)
+    upload_image_to_yadisk(img_path)
+    os.remove(img_path)
+    
+def visualize(image: np.ndarray, detection_result: processor.DetectionResult, frame_number) -> np.ndarray:
+    prev_frame = 0
+    now = datetime.now().replace(microsecond=0)
+    time_difference_in_seconds = get_difference_in_seconds(now, LAST_IMAGE_DATETIME_FILE)
+    all_probabilities = []
+    for detection in detection_result.detections:
+        # labels
+        category = detection.categories[0]
+        category_name = category.category_name
+        if category_name == "hama_burung":
+            category_name = "bird"
+        probability = round(category.score, 2)
+        result_text = str(probability)
+        #"bird"
+        #f"{category_name}"
+        all_probabilities.append(probability)
+
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        cv2.rectangle(image, start_point, end_point, BORDER_COLOR, 3)
+        text_location = (bbox.origin_x + (bbox.width - 52), _MARGIN + + _ROW_SIZE + bbox.origin_y)
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN, _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+    
+    if all_probabilities:
+        if sum(all_probabilities) / len(all_probabilities) >= 0.45 and prev_frame != frame_number:
+            if time_difference_in_seconds > TIME_BREAK:
+                random_file_name_suffix = "".join(random.choices(SYMBOLS_FOR_MAKING_RANDOM_FILE_NAME_SUFFIX, k=10))
+                img_path = 'bird_picture' + f'_{random_file_name_suffix}' + '.jpg'
+                create_image(img_path, image)
+                with open(LAST_IMAGE_DATETIME_FILE, 'w') as f:
+                    new = f.write(datetime.strftime(now, "%Y-%m-%d %H:%M:%S"))
+                    f.close()
+                prev_frame = frame_number
+    return image
